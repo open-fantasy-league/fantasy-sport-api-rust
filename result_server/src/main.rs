92f950f504c00b960974b1e7a01e35a0545727b5
The Piano Dentist <jbknight07@gmail.com>
2020-04-18 09:23:55 +0100

    starting publishing to other websockets

--- result_server/src/main.rs
+++ result_server/src/main.rs
@@ -4,16 +4,18 @@ extern crate dotenv;
 #[macro_use] // for the hlist macro
 extern crate frunk;
 extern crate frunk_core;
-use std::collections::Bound;
+use std::collections::{Bound, HashMap};
 use std::fmt;
 use chrono::{DateTime, Utc};
 use futures::{FutureExt, StreamExt};
 use warp::*;
-use tokio::sync::{mpsc};
+use tokio::sync::{mpsc, Mutex};
+use std::sync::Arc;
 mod db_pool;
 use db_pool::{pg_pool, PgPool, PgConn};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use uuid::Uuid;
 mod schema;
 mod models;
 mod db;
@@ -47,19 +49,45 @@ impl std::error::Error for InvalidRequestError{
     }
 }
 
-async fn ws_req_resp(msg: String, conn: PgConn) -> Result<String, Box<dyn std::error::Error>>{
+#[derive(Debug, Clone)]
+struct ApiError{req_type: String}
+
+// impl fmt::Display for ApiError{
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "Invalid request_type {}", self.req_type)
+//     }
+// }
+
+impl std::error::Error for ApiError{
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        // Generic error, underlying cause isn't tracked.
+        None
+    }
+}
+
+type WsConnections = Arc<Mutex<HashMap<Uuid, mpsc::UnboundedSender<Result<ws::Message, warp::Error>>>>>;
+
+async fn ws_req_resp(msg: String, conn: PgConn, ws_conns: &WsConnections) -> Result<String, Box<dyn std::error::Error>>{
     let req: WSReq = serde_json::from_str(&msg)?;
 
     match req.request_type{
         "upsert_competitions" => {
             let dr = serde_json::from_value(req.data);
-            match dr{
-            Ok(d) => match upsert_competitions(conn, d).await{
-                Ok(x) => serde_json::to_string(&x).map_err(|e| e.into()),
+            let resp: Result<String, Box<dyn std::error::Error>> = match dr{
+                Ok(d) => match upsert_competitions(conn, d).await{
+                    Ok(x) => serde_json::to_string(&x).map_err(|e| e.into()),
+                    Err(e) => Err(Box::new(e))
+                },
                 Err(e) => Err(Box::new(e))
+            };
+            if resp.is_ok(){
+                for (&uid, tx) in ws_conns.lock().await.iter_mut(){
+                    if let Err(publish) = tx.send(Ok(ws::Message::text(resp.unwrap()))){
+                        println!("Error publishing update {:?} to {}", resp, uid)
+                    };
                 }
-            Err(e) => Err(Box::new(e))
-            }
+            };
+            resp
             // serde_json::from_value(req.data).and_then(|d| async move {
             //     upsert_competitions(conn, d).await}).and_then(|r| serde_json::to_string(&r))
             //     .map_err(|e| e.into())
@@ -93,22 +121,22 @@ async fn ws_req_resp(msg: String, conn: PgConn) -> Result<String, Box<dyn std::e
             Err(Box::new(InvalidRequestError{req_type: uwotm8.to_string()}))
         }
     }
-    // No json-websocket response exists in warp yet
-    //serde_json::to_string(&vec![1, 2, 34])
 }
 
-async fn handle_ws_msg(msg: ws::Message, conn: PgConn) -> ws::Message{
-    match ws_req_resp(msg.to_str().unwrap().to_string(), conn).await{
+async fn handle_ws_msg(msg: ws::Message, conn: PgConn, ws_conns: &WsConnections) -> ws::Message{
+    match ws_req_resp(msg.to_str().unwrap().to_string(), conn, ws_conns).await{
         Ok(text) => ws::Message::text(text),
         Err(e) => generic_ws_error(e.to_string())
     }
 }
 
 
-async fn handle_ws_conn(ws: ws::WebSocket, pg_pool: PgPool){
+async fn handle_ws_conn(ws: ws::WebSocket, pg_pool: PgPool, ws_conns: WsConnections){
     // https://github.com/seanmonstar/warp/blob/master/examples/websockets_chat.rs
     let (ws_send, mut ws_recv) = ws.split();
     let (tx, rx) = mpsc::unbounded_channel();
+    let ws_id = Uuid::new_v4();
+    ws_conns.lock().await.insert(ws_id, tx);
     tokio::task::spawn(rx.forward(ws_send).map(|result| {
         if let Err(e) = result {
             eprintln!("websocket send error: {}", e);
@@ -126,21 +154,31 @@ async fn handle_ws_conn(ws: ws::WebSocket, pg_pool: PgPool){
             // pgpool get should probably be deferred until after we unwrap/get websocket message
             // but trying like this as worried about ownership of pool, moving it into funcs
             Ok(msg) => match pg_pool.get(){
-                Ok(conn) => handle_ws_msg(msg, conn).await,
+                Ok(conn) => handle_ws_msg(msg, conn, &ws_conns).await,
                 Err(e) => generic_ws_error(e.to_string())
             },
             Err(e) => {
                 eprintln!("websocket error(uid=): {}", e);
                 // If the websocket recv is broken, is it viable to try and send back through there was
                 // an error? (Don't send actual error, maybe sensitive info? Who knows?
-                tx.send(Ok(ws::Message::text("Unexpected recv error")));
+                if let Some(tx) = ws_conns.lock().await.get(&ws_id){
+                    if let Err(e) = tx.send(Ok(ws::Message::text("Unexpected recv error"))){
+                        println!("Error sending Unexpected recv error msg to {}: {:?}", &ws_id, e)
+                    };
+                }
+                ws_conns.lock().await.remove(&ws_id);
                 break;
             }
         });
         //let new_msg = format!("<User#>: {}", msg.to_str().unwrap_or("Well fuck"));
         //tx.send(Ok(ws::Message::text(new_msg.clone())));
-        tx.send(resp);
-        //user_message(my_id, msg, &users).await;
+        // Feels unnecessary locking whole map just to get our tx (we moved it into the map, so cant access variable anymore)
+        // Maybe something better
+        if let Some(tx) = ws_conns.lock().await.get(&ws_id){
+            if let Err(e) = tx.send(resp){
+                println!("Error sending regular msg to {}: {:?}", &ws_id, e)
+            };
+        }
     }
 }
 
@@ -165,10 +203,12 @@ async fn main() {
 .and_then(|_| Ok("Set data in DB"));*/
 
     // .and(pg_conn).map(|conn: PgConn|{})
-
-    let ws_router = warp::any().and(warp::ws()).map(move |ws: warp::ws::Ws|{
+    let ws_conns = Arc::new(Mutex::new(HashMap::new()));
+    // Turn our "state" into a new Filter...
+    let ws_conns_filt = warp::any().map(move || ws_conns.clone());
+    let ws_router = warp::any().and(warp::ws()).and(ws_conns_filt).map(move |ws: warp::ws::Ws, ws_conns|{
             let pool = pool.clone();
-            ws.on_upgrade(move |socket| handle_ws_conn(socket, pool))
+            ws.on_upgrade(move |socket| handle_ws_conn(socket, pool, ws_conns))
         });
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
     let league_results = warp::path!("league" / u32).map(|league_id| format!("League id {}", league_id));
