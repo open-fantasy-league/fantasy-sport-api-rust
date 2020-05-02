c9e7dd8405601ab531fd03f25093dad5f2b3ec16
The Piano Dentist <jbknight07@gmail.com>
2020-05-03 00:15:39 +0100

    whoops heres renamed subs file

---
+++ result_server/src/subscriptions.rs
@@ -0,0 +1,50 @@
+use crate::WsConnections;
+use crate::models;
+use uuid::Uuid;
+use std::collections::HashSet;
+pub struct Subscriptions{
+    pub teams: bool,
+    pub competitions: HashSet<Uuid>,
+    pub all_competitions: bool
+}
+
+impl Subscriptions{
+    pub fn new() -> Subscriptions {
+        Subscriptions{teams: false, competitions: HashSet::new(), all_competitions: false}
+    }
+}
+
+pub async fn sub_to_competitions<'a, T: Iterator<Item = &'a Uuid>>(ws_conns: &mut WsConnections, user_ws_id: Uuid, competition_ids: T){
+    if let Some(ws_user) = ws_conns.lock().await.get_mut(&user_ws_id){
+        competition_ids.for_each(|cid| {
+            println!("Adding subscription {}", cid); ws_user.subscriptions.competitions.insert(*cid);
+        });
+    };
+}
+
+pub async fn sub_to_all_competitions(ws_conns: &mut WsConnections, user_ws_id: Uuid, toggle: bool){
+    if let Some(ws_user) = ws_conns.lock().await.get_mut(&user_ws_id){
+        ws_user.subscriptions.all_competitions = toggle
+    };
+}
+
+pub async fn sub_to_teams(ws_conns: &mut WsConnections, user_ws_id: Uuid, toggle: bool){
+    if let Some(ws_user) = ws_conns.lock().await.get_mut(&user_ws_id){
+        ws_user.subscriptions.teams = toggle
+    };
+}
+
+// TODO make generic with series and matches, T and closure for competition_id? or trait for HasCompetition?
+pub fn subscribed_comps<'a>(subscriptions: &Subscriptions, all_comps: &'a Vec<models::Competition>) -> Vec<&'a models::Competition>{
+    match subscriptions.all_competitions{
+        // turn from &Vec<models::Competition> into Vec<&models::Competition>
+        // Passing in &Vec to func, so that publish and send response can 'share' competition. i.e. publishing doesnt consume it.
+        // However is probably simpler to set up so can just clone it, and this func mvoes Vec, rather than ref
+        true => all_comps.iter().collect(),
+        false => {
+            all_comps.iter()
+            .filter(|c| subscriptions.competitions.contains(&c.competition_id))
+            .collect()
+        }
+    }
+}
